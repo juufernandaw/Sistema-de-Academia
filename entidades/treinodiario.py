05783367f73474fe80fcab9a69c60b7173818068
Diego Goiana <diegovsg96@gmail.com>
2022-06-11 15:00:08 -0300

    personal e treinoDiario

---
+++ entidades/treinodiario.py
@@ -0,0 +1,33 @@
+from entidades.treino import Treino
+from entidades.aluno import Aluno
+from datetime import date
+
+
+class TreinoDiario():
+    def __init__(self, aluno: Aluno, data: date, treinos: Treino):
+        self.__aluno = aluno
+        self.__data = data
+        self.__treinos = []
+
+    @property
+    def aluno(self):
+        return self.__aluno
+
+    @aluno.setter
+    def aluno(self, aluno: Aluno):
+        if isinstance(aluno, Aluno):
+            self.__aluno = aluno
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data: date):
+        if isinstance(data, date):
+            self.__data = data
+
+    def treinos(self, treinos: Treino):
+        if isinstance(treinos, Treino):
+            pass
+
