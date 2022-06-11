773cbac24cd928ce9e27bcd2787192f4b6a4d1a2
juufernandaw <ajulia.fernanda80@gmail.com>
2022-06-11 15:23:27 -0300

    Criação métodos

--- controladores/controladoraluno.py
+++ controladores/controladoraluno.py
@@ -1,1 +1,31 @@
+from telas.telaaluno import TelaAluno
+from entidades.aluno import Aluno
 
+class ControladorAluno():
+
+    def __init__(self, controlador_sistema):
+        self.__controlador_sistema = controlador_sistema
+        self.__alunos = []
+        self.__manter_tela = True
+        self.__tela_aluno = TelaAluno()
+
+    def incluir_aluno():
+        pass
+
+    def alterar_aluno():
+        pass
+
+    def excluir_aluno():
+        pass
+
+    def consultar_aluno():
+        pass
+
+    def listar_alunos():
+        pass
+
+    def abre_tela_inicial():
+        pass
+
+    def retornar():
+        pass
