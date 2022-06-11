c227f8fe77ece23337d1d2196b769d30501abf3c
Diego Goiana <diegovsg96@gmail.com>
2022-06-11 14:23:43 -0300

    Personal: ctr, tela e entidade

--- controladores/controladorpersonaltrainer.py
+++ controladores/controladorpersonaltrainer.py
@@ -1,1 +1,30 @@
+from entidades.personaltrainer import PersonalTrainer
+from controladores.controladorsistema import ControladorSistema
+from telas.telapersonaltrainer import TelaPersonal
 
+
+class ControladorPersonalTrainer():
+    def __init__(self, controlador_sistema):
+        self.__tela_personal = TelaPersonal()
+        self.__manter_tela = bool
+        self.__controlador_sistema = controlador_sistema
+
+    def alterar_personal(self, cpf: str, nome: str, login: str, senha: str):
+        if isinstance(cpf, str) and isinstance(nome, str) and isinstance(login, str) and \
+                isinstance(senha, str):
+            pass
+
+    def consultar_personal(self, cpf: str, nome: str, login: str, senha: str):
+        if isinstance(cpf, str) and isinstance(nome, str) and isinstance(login, str) and \
+                isinstance(senha, str):
+            pass
+
+    def abre_tela_inicial(self):
+        pass
+
+    def retornar(self):
+        pass
+
+    def logar(self, login: str, senha: str):
+        if isinstance(login, str) and isinstance(senha, str):
+            pass
