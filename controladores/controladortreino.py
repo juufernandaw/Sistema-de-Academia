50f7f9c61b61a9b536f8a29e6e37be39846beb3d
juufernandaw <ajulia.fernanda80@gmail.com>
2022-06-11 15:57:35 -0300

    Update controladortreino.py

--- controladores/controladortreino.py
+++ controladores/controladortreino.py
@@ -9,3 +9,20 @@ class ControladorTreino():
         self.__manter_tela = True
         self.__tela_treino = TelaTreino()
 
+    def incluir_treino():
+        pass
+
+    def excluir_treino():
+        pass
+
+    def alterar_treino():
+        pass
+
+    def consultar_treino():
+        pass
+
+    def vincular_treino_aluno():
+        pass
+
+    def retornar():
+        pass
