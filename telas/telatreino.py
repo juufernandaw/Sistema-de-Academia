d33e3032a4d56bedf23c324d5e320892911602a2
Diego Goiana <diegovsg96@gmail.com>
2022-06-12 10:26:45 -0300

    troca da telatreino para treinodiario
    confudi

--- telas/telatreino.py
+++
@@ -1,7 +0,0 @@
-
-class TelaTreino():
-    def __init__(self):
-        pass
-
-    def mostrarTela_desenpenho(self):
-        pass
---
+++ telas/telatreinodiario.py
@@ -0,0 +1,7 @@
+
+class TelaTreino():
+    def __init__(self):
+        pass
+
+    def mostrarTela_desenpenho(self):
+        pass
