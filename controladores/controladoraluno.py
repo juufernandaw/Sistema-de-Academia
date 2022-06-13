777dc3555bfd082f6a4f675107f81964c84f3161
juufernandaw <ajulia.fernanda80@gmail.com>
2022-06-12 22:35:35 -0300

    implementando métodos aluno

--- controladores/controladoraluno.py
+++ controladores/controladoraluno.py
@@ -1,5 +1,5 @@
-from TrabalhoPOO.telas.telaaluno import TelaAluno
-from TrabalhoPOO.entidades.aluno import Aluno
+from telas.telaaluno import TelaAluno
+from entidades.aluno import Aluno
 
 
 class ControladorAluno():
@@ -11,20 +11,40 @@ class ControladorAluno():
         self.__tela_aluno = TelaAluno()
 
     def incluir_aluno(self):
-        pass
+        dados_aluno = self.__tela_aluno.pega_dados_aluno() #implementar método pega_dados_aluno(): usuário digita tds as infos 
+        aluno = Aluno(dados_aluno["nome"], dados_aluno["login"], dados_aluno["senha"], dados_aluno["cpf"])
+        self.__alunos.append(aluno)
 
     def alterar_aluno(self):
-        pass
+        cpf = self.__tela_aluno.seleciona_aluno() #implementar seleciona_aluno()
+        aluno = self.consultar_aluno(cpf) 
+        if (aluno is not None):
+            novos_dados_aluno = self.__tela_aluno.pega_dados_aluno()
+            aluno.nome = novos_dados_aluno["nome"]
+            aluno.login = novos_dados_aluno["login"]
+            aluno.senha = novos_dados_aluno["senha"]
+            aluno.cpf = novos_dados_aluno["cpf"]
+        else:
+            self.__tela_aluno.mostrar_msg("ATENCAO: aluno não existente")
 
     def excluir_aluno(self):
-        pass
+        cpf_aluno = self.__tela_aluno.seleciona_aluno() #implementar seleciona_aluno()
+        aluno = self.consultar_aluno(cpf_aluno) 
+        if (aluno is not None):
+            self.__alunos.remove(aluno)
+        else:
+            self.__tela_aluno.mostrar_msg("ATENCAO: aluno não existente")
 
-    def consultar_aluno(self):
-        pass
+    def consultar_aluno(self, cpf: str):
+        for aluno in self.__alunos:
+            if aluno.cpf == cpf:
+                return aluno
+        return None
 
     def listar_alunos(self):
-        pass
-
+        for aluno in self.__alunos:
+            self.__tela_aluno.mostrar_aluno({"nome": aluno.nome, "login": aluno.login, "senha": aluno.senha, "cpf": aluno.cpf})
+            #implementar metodo mostrar aluno
     def abre_tela_inicial(self):
         pass
 
--- entidades/aluno.py
+++ entidades/aluno.py
@@ -1,4 +1,4 @@
-from usuario import Usuario
+from entidades.usuario import Usuario
 
 
 class Aluno(Usuario):
