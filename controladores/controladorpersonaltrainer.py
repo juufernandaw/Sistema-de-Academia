e9340aa4229d6df41171a4c089fb1c74b59566b8
Diego Goiana <86124730+diegovsg@users.noreply.github.com>
2022-06-13 21:59:55 -0300

    Update controladorpersonaltrainer.py

--- controladores/controladorpersonaltrainer.py
+++ controladores/controladorpersonaltrainer.py
@@ -1,41 +1,21 @@
 from TrabalhoPOO.entidades.personaltrainer import PersonalTrainer
 from TrabalhoPOO.controladores.controladorsistema import ControladorSistema
 from TrabalhoPOO.telas.telapersonaltrainer import TelaPersonalTrainer
-
+from TrabalhoPOO.telas.telasistema import TelaSistema
 
 class ControladorPersonalTrainer():
     def __init__(self, controlador_sistema):
         self.__tela_personal = TelaPersonalTrainer()
         self.__manter_tela = bool
         self.__controlador_sistema = controlador_sistema
-        self.__dados_personal = {cpf: "123456789", nome: "Judi Goilang", login: "Adm", senha: "adm", habilitacao: "01"}
+        self.__tela_sistema = TelaSistema()
+        self.__personal = PersonalTrainer(123456789, Judi Goilang, "Adm", "Adm", '01')
+        #self.__dados_personal = {'cpf': "123456789", 'nome': "Judi Goilang", 'login': "Adm", 'senha': "Adm"", 'habilitacao': "01"}#instancia da classe
 
-    def alterar_personal(self, cpf: str, nome: str, login: str, senha: str, habilitacao: str):
-        if isinstance(cpf, str) and isinstance(nome, str) and isinstance(login, str) and \
-                isinstance(senha, str) and isinstance(habilitacao, str):
-            print("O que você deseja alterar")
-            print("1. CPF: 2. Nome: 3.Login: 4.Senha: 0.tela inicial")
-            opcao = int(input())
-            while opcao != 0:
-                if opcao == 1:
-                    print("alterar cpf:")
-                    cpf = input()
-                    self.__dados_personal.update({nome:"cpf"})
-                elif opcao == 2:
-                    print("Alterar nome")
-                    nome = input()
-                elif opcao == 3:
-                    print("alterar login:")
-                    login = input()
-                elif opcao == 4:
-                    print("Alterar senha")
-                    senha = input()
-                    self.
-                elif opcao == 0:
-                   self.abre_tela_inicial()
-                else:
-                    print("Digite o valor correto")
-                    continue
+    def alterar_personal(self):
+        alterar, opcao = self.__tela_sistema.mostrar_tela_alterar_dados() #get e setter modificando com isso
+        switcher = {1: self.__personal.cpf, 2: self.__personal.nome, 3: self.__personal.login, 4: self.__personal.senha, 5: self.__personal.habilitacao}
+        switcher[opcao]() = alterar #TESTAR ESSA loucura
 
     def consultar_personal(self, cpf: str, nome: str, login: str, senha: str):
         if isinstance(cpf, str) and isinstance(nome, str) and isinstance(login, str) and \
@@ -48,7 +28,9 @@ class ControladorPersonalTrainer():
     def retornar(self):
         pass
 
-    def verificar_login_senha(self, login: str, senha: str):
+    def verificar_login_senha(): #VERIFICAR 
+        login, senha = self.__tela_sistema.logar()
         if isinstance(login, str) and isinstance(senha, str):
-            
-            return True
+            if self.__personal.login == login and self.__personal.senha == senha:
+                return True
+        
