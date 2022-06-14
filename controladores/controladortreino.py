bf07c3979c6adc4e60de76039696dfe561246471
juufernandaw <ajulia.fernanda80@gmail.com>
2022-06-14 01:08:24 -0300

    método incluir treino

--- controladores/controladortreino.py
+++ controladores/controladortreino.py
@@ -18,14 +18,16 @@ class ControladorTreino():
         return self.__tipos_exercicio
 
     def incluir_treino(self):
-        #pedir o nome do treino e os exercicios
-        novo_treino = self.__tela_treino.montar_treino()
-        novo_exercicio = self.__tela_treino.montar_exercicio()
-        while novo_treino==1: #pra criar novo treino
-            novo_exercicio = self.__tela_treino.montar_exercicio()
-            treino = Treino(nome) #instancia o treino
+        novo_treino, nome_treino = self.__tela_treino.montar_treino() #pedir o nome do treino e os exercicios
+        while (novo_treino==1) and (nome_treino is not None): #pra criar novo treino
+            treino = Treino(nome_treino) #instancia o treino
+            novo_exercicio, dados_exercicio = self.__tela_treino.montar_exercicio()
             while novo_exercicio==1: #pra criar novo exercicio
-                treino.incluir_exercicio(dados_treino["nome"], dados_treino["serie"], dados_treino["repeticao"], dados_treino["tempo_descanso"], self.__tipos_exercicio[dados_treino["tipo_exercicio"]])
+                treino.incluir_exercicio(dados_exercicio["nome"], dados_exercicio["serie"], dados_exercicio["repeticao"],\
+                dados_exercicio["tempo_descanso"], self.__tipos_exercicio[dados_exercicio["tipo_exercicio"]])
+                self.vincular_treino_aluno(treino) #chama o método p vincular o treino com o aluno
+                novo_exercicio, dados_exercicio = self.__tela_treino.montar_exercicio()
+            novo_treino, nome_treino = self.__tela_treino.montar_treino()
         #chamar método vincular_treino_aluno
 
     def excluir_treino(self):
@@ -37,7 +39,8 @@ class ControladorTreino():
     def consultar_treino(self):
         pass
 
-    def vincular_treino_aluno(self):
+    def vincular_treino_aluno(self, treino: Treino):
+        #chama o método seleciona_aluno() aí dá um append nos exercicios[] do Aluno
         pass
 
     def retornar(self):
--- telas/telatreino.py
+++ telas/telatreino.py
@@ -3,20 +3,27 @@ class TelaTreino():
     def mostrar_tela_treino():
         pass
 
-    def montar_treino():
-        nome = input("Nome do treino:")
-        novo_exercicio = input("Deseja cadastrar um novo exercício? 1- Sim 2-Não")
-        return novo_exercicio
+    def montar_treino(self):
+        opcao = int(input("Deseja cadastrar um novo treino? 1- Sim 2- Não"))
+        if opcao == 1:
+            nome_treino = input("Nome do treino:")
+        else:
+            nome_treino = None
+        return opcao, nome_treino
+            
         #cadastro exercicio
     
     def montar_exercicio(self,lista_tipos=[]):
-        nome = input("Nome:")
-        serie = input("Série:")
-        repeticao = input("Repetição:")
-        tempo_descanso = input("Tempo de descanso:")
-        tipo_exercicio = input("Escolha o tipo de exercício: 0) Muscular - Superior 1) Muscular - Inferior 2) Cardiovascular")#chamar a lista de tipos_chamados
-        #retorna o exercicio criado
-        return {"nome":nome, "serie":serie, "repeticao":repeticao, "tempo_descanso":tempo_descanso, "tipo_exercicio":tipo_exercicio}
+        opcao = int(input("Deseja cadastrar um novo exercício? 1- Sim 2- Não"))
+        if opcao == 1:
+            nome = input("Nome:")
+            serie = input("Série:")
+            repeticao = input("Repetição:")
+            tempo_descanso = input("Tempo de descanso:")
+            tipo_exercicio = input("Escolha o tipo de exercício: 0) Muscular - Superior 1) Muscular - Inferior 2) Cardiovascular")#chamar a lista de tipos_chamados
+        else:
+            nome, serie, repeticao, tempo_descanso, tipo_exercicio = None
+        return opcao,{"nome":nome, "serie":serie, "repeticao":repeticao, "tempo_descanso":tempo_descanso, "tipo_exercicio":tipo_exercicio}
 
 
     
