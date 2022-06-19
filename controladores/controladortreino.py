ce020ded9c8dccfb19f6c0f8b2248b8d46a78ebb
juufernandaw <ajulia.fernanda80@gmail.com>
2022-06-19 15:40:06 -0300

    Update controladortreino.py

--- controladores/controladortreino.py
+++ controladores/controladortreino.py
@@ -91,6 +91,7 @@ class ControladorTreino():
             self.__tela_treino.mostrar_tela_treino(treino)
         else:    
             self.__tela_treino.mostrar_msg("ATENCAO: treino não existente")
+        return self.abre_tela_funcoes_treino()
 
     def abre_tela_funcoes_treino(self):
         lista_opcoes = {1: self.incluir_treino, 2: self.alterar_treino,
